eeea2cb283975d58e7e7b3f26a0b77fbea5e33c1
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-01-15 21:26:21 -0500

    fixed bug

--- challenges/chal1/longest_substring.py
+++ challenges/chal1/longest_substring.py
@@ -10,24 +10,34 @@ def longest_substring(string):
     visited = {}
     longest_string = ""
     
-    for i in range(len(string)):        
-        last_equal_char_known_pos = 0
-        
+    last_known_repeated = 0
+    
+    for i in range(len(string)):    
         if string[i] not in visited:
             visited[string[i]] = i
+            
+            contestant_longest_string = i - last_known_repeated + 1
+            
+            if contestant_longest_string > len(longest_string):
+                longest_string = string[last_known_repeated:i+1]
+                #print("1:", longest_string)
+            
         else:
             contestant_longest_string = i - visited[string[i]]
             
             if contestant_longest_string > len(longest_string):
                 longest_string = string[visited[string[i]]:i]
+                print("2: ", longest_string)
+            
+            last_known_repeated = visited[string[i]]+1
             
             visited[string[i]] = i
     
     if longest_string == "":
         longest_string = string
             
-    return longest_string
+    return len(longest_string)
     
         
 
-print(longest_substring("abcabcbb"))
+print(longest_substring("
